3fe50c0437a48b1299a1f2d973fe77ece1213ffd
Loler-proger <ngannou252@mail.ru>
2025-11-02 23:06:34 +0300

    Create player.entity.ts

---
+++ apps/backend/src/lobby/entities/player.entity.ts
@@ -0,0 +1,11 @@
+import type { User } from '../../users/entities/user.entity';
+import type { Question } from '../../decks/entities/question.entity';
+
+/**
+ * Сущность "Игрок"
+ */
+export interface Player extends User {
+  question: Question;
+  score: number;
+  liarProbability: number;
+}
