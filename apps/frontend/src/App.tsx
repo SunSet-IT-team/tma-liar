8e0d978ed9346711ef19b12ed75db433d43bc539
Kurban <sweet18892214@mail.ru>
2025-11-15 12:57:50 +0300

    Добавил кастомный инпут

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,16 +1,23 @@
 import { CustomButton } from './components/button/CustomButton';
 import Typography from './components/connectFont/Typography';
+import { TextInput } from './components/input/TextInput';
 import fonts from './shared/fonts/fonts.module.scss'
+import cl from './shared/UI/Aboba/index.module.scss'
 
 function App() {
   return (
     <div className={fonts.fontFamily}>
-      <Typography variant='caption' className={fonts.typographyTestStyle}>
+      <Typography variant='caption' className={cl.typographyTestStyle}>
         Какой-то текст
       </Typography>
       <CustomButton variant='buttonUnderline'>
         <span>Кнопка с подчеркиванием</span>
       </CustomButton>
+      <CustomButton variant='buttonText'>
+        <span>Кнопка без подчеркиния</span>
+      </CustomButton>
+      <TextInput placeholder='Task' />
+      <TextInput placeholder='Вылизываем кота !' className={cl.inputStyle} />
     </div>
   );
 }
