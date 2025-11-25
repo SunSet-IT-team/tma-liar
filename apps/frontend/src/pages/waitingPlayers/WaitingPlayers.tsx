37a838e65a9e116baea74c439ebdcb7e353efbbf
Kurban <sweet18892214@mail.ru>
2025-11-25 17:33:31 +0300

    Добавил страницу ожидания игроков

---
+++ apps/frontend/src/pages/waitingPlayers/WaitingPlayers.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react"
+import { Typography } from "../../shared/Typography/Typography"
+import { Header } from "../../widgets/header/Header"
+import styles from './style/waitingPlayersStyle.module.scss'
+import waitingIcon from '../../assets/icons/waitingIcon.svg'
+import waitingCircle from '../../assets/icons/waitingCircle.svg'
+
+export const WaitingPlayers: FC = () => {
+  return (
+    <div className={styles.container}>
+      <Header className={styles.header} />
+      <h1 className={styles.title}>Ждем!</h1>
+      <Typography>Других игроков</Typography> 
+      <img src={waitingIcon} alt="" className={styles.waitingIcon} />
+      <img src={waitingCircle} alt="" className={styles.waitingCircle} />
+      <Typography className={styles.waitingText}>Уже скоро?</Typography>
+    </div>
+  )
+}
