76d9650dc140e0b91e8c6436cef1b0d2deae14ab
Гусейн <gusejng324@gmail.com>
2025-12-05 16:54:04 +0300

    add AnswerSolved page

---
+++ apps/frontend/src/pages/AnswerSolved/index.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react"
+import { UserBadge } from "../../entities/user/ui/UserBadge"
+import { Button } from "../../shared/ui/Button"
+import { Container } from "../../shared/ui/Container"
+import { Timer } from "../../shared/ui/Timer"
+import { Typography } from "../../shared/ui/Typography"
+import { Header } from "../../widgets/Header"
+import styles from './style/answerSolvedStyle.module.scss'
+
+export const AnswerSolved: FC = () => {
+  return (
+    <Container>
+      <Header className={styles.header} />
+      <div className={styles.content}>
+        <Typography as='h1' variant='titleLarge'>
+          Лжец
+          <Typography as='span' variant='titleLarge' className={styles.titleItem}>?</Typography>
+        </Typography>
+        <UserBadge id={1} name="Хаах Татар" className={styles.liarPlayer} />
+        <Typography className={styles.questionLiar}>Вопрос заданный лжецу</Typography>
+      </div>
+      
+      <div className={styles.answersBtns}>
+        <Button className={styles.answersBtn}>Не верю</Button>
+        <Button className={styles.answersBtn}>Верю</Button>
+      </div>
+      <Button className={styles.fixAnswerBtn}>Зафиксировать</Button>
+      <Timer time={10} />
+    </Container>
+  )
+}
